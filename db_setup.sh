cb7d5f1ec23dc0cf7503213bf15d23236d7c2170
Josh McArthur <joshua.mcarthur@gmail.com>
2015-02-20 13:29:42 +1300

    Setup PostGIS in setup script

--- db_setup.sh
+++ db_setup.sh
@@ -1,2 +1,2 @@
 createdb wellington_transit
-psql wellington_transit -c 'create schema "1"; alter database wellington_transit set search_path="1";'
+psql wellington_transit -c 'create extension postgis;create schema "1"; alter database wellington_transit set search_path="1";'
